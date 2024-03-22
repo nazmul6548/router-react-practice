9f217a9f5597e2134abf3d9f107bd3363ecffb48
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-22 12:31:26 +0600

    task

--- src/Components/Errorpage/Errorpage.jsx
+++ src/Components/Errorpage/Errorpage.jsx
@@ -1,9 +1,21 @@
+import { Link, useRouteError } from "react-router-dom"
 
 
 function Errorpage() {
+    const error = useRouteError();
+    console.log(error);
   return (
     <div>
         <h1>oops! you provide right path </h1>
+        <p>{error.statusText || error.message}</p>
+        {
+            error.status === 404 && 
+            <div>
+                <h3>page not found</h3>
+                <p>go back where you from</p>
+                <Link to="/"> <button>Home</button> </Link>
+            </div>
+        }
     </div>
   )
 }
